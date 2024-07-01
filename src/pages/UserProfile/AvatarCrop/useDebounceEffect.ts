ed45d4a06cbd51f5ad9f600ed60f574e9f3dc08d
HuuTrong <huutrong1101@gmail.com>
2024-07-02 00:21:39 +0700

    update code

--- src/pages/UserProfile/AvatarCrop/useDebounceEffect.ts
+++ src/pages/UserProfile/AvatarCrop/useDebounceEffect.ts
@@ -1,9 +1,9 @@
 import { useEffect, DependencyList } from 'react'
 
-export function useDebounceEffect(fn: () => void, waitTime: number, deps?: DependencyList) {
+export function useDebounceEffect(fn: () => void, waitTime: number, deps: DependencyList = []) {
   useEffect(() => {
     const t = setTimeout(() => {
-      fn.apply(undefined, deps)
+      fn()
     }, waitTime)
 
     return () => {
